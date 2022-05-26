91dd277f758e2b4f42b5726bf04ee5ad67d6cbce
Смирнов Дмитрий (OSS) <dmitry.g.smirnov@megafon.ru>
2022-05-26 18:03:35 +0300

    init

--- dnp1_test.js
+++ dnp1_test.js
@@ -1,2 +1,7 @@
 open main page
-click on data-test-item-id="AddressPlan"
+I.click('Адресный план'); //click on data-test-item-id="AddressPlan"
+
+I.waitForElement('#agree_button', 30); // secs
+// clicks a button only when it is visible
+//https://codecept.io/helpers/Playwright/#clearcookie
+I.click('#agree_button');
